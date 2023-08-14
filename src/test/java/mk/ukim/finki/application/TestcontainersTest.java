05d2895cc5cf8b07ff530a95965508f2a9903d01
Petar <petarkuzmanoski96@gmail.com>
2023-08-14 21:56:12 +0100

    Testcontaners add.

---
+++ src/test/java/mk/ukim/finki/application/TestcontainersTest.java
@@ -0,0 +1,25 @@
+package mk.ukim.finki.application;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public class TestcontainersTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("application-dao-unit-test")
+                    .withUsername("username")
+                    .withPassword("password");
+
+    @Test
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
+}
