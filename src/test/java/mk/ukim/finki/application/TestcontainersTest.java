5d789590ae99705ccb5c555ab4e3586c854aadc4
Petar <petarkuzmanoski96@gmail.com>
2023-08-15 00:28:31 +0100

    Add dynamic property source

--- src/test/java/mk/ukim/finki/application/TestcontainersTest.java
+++ src/test/java/mk/ukim/finki/application/TestcontainersTest.java
@@ -1,6 +1,10 @@
 package mk.ukim.finki.application;
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -8,6 +12,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
+@SpringBootTest
 public class TestcontainersTest {
 
     @Container
@@ -17,9 +22,38 @@ public class TestcontainersTest {
                     .withUsername("username")
                     .withPassword("password");
 
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add(
+                "spring.datasource.url",
+                postgreSQLContainer::getJdbcUrl
+        );
+        registry.add(
+                "spring.datasource.username",
+                postgreSQLContainer::getUsername
+        );
+        registry.add(
+                "spring.datasource.password",
+                postgreSQLContainer::getPassword
+        );
+    }
+
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
+        assertThat(postgreSQLContainer.isHealthy()).isTrue();
     }
+
+    @Test
+    void canApplyDbMigrationsWithFlyway() {
+        Flyway flyway = Flyway.configure().dataSource(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()
+        ).load();
+        flyway.migrate();
+    }
+
+
 }
