7f885cb893fb710796465f8eb10cebaaf5d97213
Petar <petarkuzmanoski96@gmail.com>
2023-08-23 01:43:08 +0100

    Debugging CD.

--- backend/src/main/java/mk/ukim/finki/application/Main.java
+++ backend/src/main/java/mk/ukim/finki/application/Main.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Bean;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-
     }
 
     @Bean
