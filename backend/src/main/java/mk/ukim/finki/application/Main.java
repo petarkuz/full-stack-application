0800531c3f633062afb2da976084c65cf149114f
Petar <petarkuzmanoski96@gmail.com>
2023-08-23 01:02:44 +0100

    Debugging CD.

--- backend/src/main/java/mk/ukim/finki/application/Main.java
+++ backend/src/main/java/mk/ukim/finki/application/Main.java
@@ -8,8 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
-
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
