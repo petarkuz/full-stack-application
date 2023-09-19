7a11b3c2ebae2d000cd920dfb556b9fb049b2a5b
Petar <petarkuzmanoski96@gmail.com>
2023-09-20 00:14:44 +0100

    Login React.

--- frontend/react/src/services/client.js
+++ frontend/react/src/services/client.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
+const getAuthConfig = () => ({
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`
+    }
+})
+
 export const getCustomers = async () => {
     try {
-        return await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/v1/customers`)
+        return (await axios.get(
+                `${import.meta.env.VITE_API_BASE_URL}/api/v1/customers`,
+                getAuthConfig())
+        )
     } catch (err) {
         throw err;
     }
@@ -21,7 +30,8 @@ export const saveCustomer = async (customer) => {
 
 export const deleteCustomer = async (id) => {
     try {
-        return await axios.delete(`${import.meta.env.VITE_API_BASE_URL}/api/v1/customers/${id}`);
+        return await axios.delete(`${import.meta.env.VITE_API_BASE_URL}/api/v1/customers/${id}`,
+            getAuthConfig());
     } catch (err) {
         throw err;
     }
@@ -30,7 +40,18 @@ export const deleteCustomer = async (id) => {
 export const updateCustomer = async (id, update) => {
     try {
         return (await axios.put(`${import.meta.env.VITE_API_BASE_URL}/api/v1/customers/${id}`,
-            update)
+                update,
+                getAuthConfig())
+        )
+    } catch (err) {
+        throw err;
+    }
+}
+
+export const login = async (usernameAndPassword) => {
+    try {
+        return (await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/login`,
+                usernameAndPassword)
         )
     } catch (err) {
         throw err;
